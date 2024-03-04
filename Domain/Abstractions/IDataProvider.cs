a2e03e810d473e9b51ef35eb975c5eb9b4288677
Egor <egor_spaik05@mail.ru>
2024-03-04 23:54:45 +0300

    Domain project created. DataProvider service added

---
+++ Domain/Abstractions/IDataProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace OzonSales.Domain.Abstractions;
+
+public interface IUnitOfWork<TEntity>
+{
+    Task GetAsync();
+    Task UpdateAsync();
+    Task<ulong> CreateAsync();
+    Task RemoveAsync(ulong id);
+}
---
+++ Domain/TxtDataProvider.cs
@@ -0,0 +1,26 @@
+﻿using OzonSales.Domain.Abstractions;
+
+namespace OzonSales.Domain;
+
+public class DataProvider<TEntity> : IDataProvider<TEntity>
+{
+    public Task GetAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ulong> CreateAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveAsync(ulong id)
+    {
+        throw new NotImplementedException();
+    }
+}
