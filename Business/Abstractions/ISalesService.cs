7b38c02668ed40699eee9e72df866525a6a0e48f
Egor <egor_spaik05@mail.ru>
2024-03-05 00:53:38 +0300

    Business project created. ISalesService added.

---
+++ Business/Abstractions/ISalesService.cs
@@ -0,0 +1,9 @@
+﻿namespace OzonSales.Business.Abstractions;
+
+public interface ISalesService
+{
+    // TODO: create divided class for ADS calculation result
+    Task<decimal> GetADSAsync();
+    Task<uint> GetSalesPredictionAsync();
+    Task<uint> GetDemandAsync();
+}
---
+++ Business/SalesService.cs
@@ -0,0 +1,21 @@
+﻿using OzonSales.Business.Abstractions;
+
+namespace OzonSales.Business;
+
+public class SalesService : ISalesService
+{
+    public Task<decimal> GetADSAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<uint> GetSalesPredictionAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<uint> GetDemandAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
